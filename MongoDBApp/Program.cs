740952199cda5e56155fcb2354c7ab18e5412d5f
Arthur B. <arthur.b@cloudberrylab.com>
2016-03-28 18:55:50 +0300

    first Iteration

---
+++ MongoDBApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Configuration;
+using MongoDB.Driver;
+
+namespace MongoDBApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string con = ConfigurationManager.ConnectionStrings["MongoDb"].ConnectionString;
+            MongoClient client = new MongoClient(con);
+            GetDatabaseNames(client);
+            Console.ReadLine();
+        }
+
+        private static async void GetDatabaseNames(MongoClient client)
+        {
+            using (var cursor = await client.ListDatabasesAsync())
+            {
+                var databaseDocuments = await cursor.ToListAsync();
+                foreach (var databaseDocument in databaseDocuments)
+                {
+                    Console.WriteLine(databaseDocument["name"]);
+                }
+            }
+        }
+    }
+}
