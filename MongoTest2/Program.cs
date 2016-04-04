5e44a86531bdca001cde309cbe4995d4b140d1eb
Arthur B. <arthur.b@cloudberrylab.com>
2016-04-04 18:42:49 +0300

    connected to MongoDB

--- MongoTest2/Program.cs
+++ MongoTest2/Program.cs
@@ -1,9 +1,30 @@
-﻿namespace MongoTest2
+﻿using System;
+using System.Configuration;
+using MongoDB.Driver;
+
+namespace MongoTest2
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string con =
+                ConfigurationManager.ConnectionStrings["MongoTest2.Properties.Settings.MongoDb"].ConnectionString;
+            MongoClient client = new MongoClient(con);
+            GetDatabaseNames(client);
+            Console.ReadLine();
+        }
+
+        private static async void GetDatabaseNames(MongoClient client)
+        {
+            using (var cursor = await client.ListDatabasesAsync())
+            {
+                var databaseDocuments = await cursor.ToListAsync();
+                foreach (var databaseDocument in databaseDocuments)
+                {
+                    Console.WriteLine(databaseDocument["name"]);
+                }
+            }
         }
     }
 }
