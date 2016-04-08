2e9294e8b34f8d9b2d8368438694f7704d1f079a
Arthur B. <arthur.b@cloudberrylab.com>
2016-04-08 20:53:11 +0300

    added main page

--- MongoMVC/Controllers/HomeController.cs
+++ MongoMVC/Controllers/HomeController.cs
@@ -1,30 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Threading.Tasks;
 using System.Web.Mvc;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoMVC.Models;
 
 namespace MongoMVC.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        ComputerContext db = new ComputerContext();
+
+        public async Task<ActionResult> Index(ComputerFilter cFilter)
         {
-            return View();
+            var computers = await FilterAsync(cFilter);
+            var model = new ComputerList { Computers = computers, Filter = cFilter };
+            return View(model);
         }
 
-        public ActionResult About()
+        public async Task<IEnumerable<Computer>> FilterAsync(ComputerFilter cFilter)
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            var builder = Builders<Computer>.Filter;
+            var filters = new List<FilterDefinition<Computer>>();
+            if (!String.IsNullOrWhiteSpace(cFilter.ComputerName))
+            {
+                filters.Add(builder.Eq("Name", new BsonRegularExpression(cFilter.ComputerName)));
+            }
+            if (cFilter.Year.HasValue)
+            {
+                filters.Add(builder.Eq("Year", cFilter.Year));
+            }
+            return await db.Computers.Find(builder.And(filters)).ToListAsync();
         }
     }
 }
